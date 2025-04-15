3b718086d0112ef5a383d3cd39fe61cba11499e3
qaiswardag <qais.wardag@outlook.com>
2025-04-15 09:21:21 +0200

    Enhance ReadJSON method to provide detailed error handling for JSON decoding

--- toolkit/jsontool.go
+++ toolkit/jsontool.go
@@ -3,6 +3,7 @@ package toolkit
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
 )
@@ -35,7 +36,37 @@ func (t Tools) ReadJSON(w http.ResponseWriter, r *http.Request, data interface{}
 	err := dec.Decode(data)
 
 	if err != nil {
-		return err
+
+		if err == io.EOF {
+			// Handle empty body error
+			return errors.New("body must not be empty")
+		}
+
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		if errors.As(err, &syntaxError) {
+			// Handle syntax error
+			return fmt.Errorf("1 body contains badly-formed JSON (at character %d): %w", syntaxError.Offset, err)
+
+		}
+
+		if errors.As(err, &unmarshalTypeError) {
+			// Handle unmarshal type error
+
+			return fmt.Errorf(
+				"2 body contains an invalid value for the field %q",
+				unmarshalTypeError.Field,
+			)
+		}
+
+		if errors.As(err, &invalidUnmarshalError) {
+			// Handle invalid unmarshal error
+			return fmt.Errorf("3 body contains an invalid JSON structure: %w", err)
+		}
+
+		return fmt.Errorf("4 request body must be valid JSON: %w", err)
 	}
 
 	err = dec.Decode(&struct{}{})
